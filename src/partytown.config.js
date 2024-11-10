8cc4a9df016f6857b3dff9382c1e2df9555caf44
bomboonsan <bomboonsan@gmail.com>
2024-11-10 18:32:22 +0700

    optimize-ads-8

--- src/partytown.config.js
+++
@@ -1,7 +0,0 @@
-import { start } from '@partytown/core';
-
-start({
-    debug: false,
-    forward: ['adsbygoogle'],
-    lib: 'https://cdn.jsdelivr.net/npm/@partytown/html@latest/dist/partytown.js',
-});
