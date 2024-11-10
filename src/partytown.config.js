a1cacaff3c16ee3070a9a49f8098a805a28bce73
bomboonsan <bomboonsan@gmail.com>
2024-11-10 17:37:47 +0700

    optimize-ads4

---
+++ src/partytown.config.js
@@ -0,0 +1,7 @@
+import { start } from '@partytown/core';
+
+start({
+    debug: false,
+    forward: ['adsbygoogle'],
+    lib: 'https://cdn.jsdelivr.net/npm/@partytown/html@latest/dist/partytown.js',
+});
