e538f0794abfad1c092320e799a2815c012b72b8
bomboonsan <bomboonsan@gmail.com>
2024-11-11 13:10:43 +0700

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,11 @@ export default defineConfig({
       }), 
       tailwind(), 
       react(), 
-      partytown()
+      partytown({
+        config: {
+          forward: ['dataLayer.push'],
+        },
+      })
     ],
     output: 'static',
 });
