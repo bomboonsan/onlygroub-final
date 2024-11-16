c72c4e69f076dfc78c9eee9c6879d581300ef7df
bomboonsan <bomboonsan@gmail.com>
2024-11-16 15:35:30 +0700

    updateSeo

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/Page_BVz5pLUZ.mjs
+++
@@ -1,8 +0,0 @@
-import { c as createComponent, r as renderTemplate, m as maybeRenderHead, e as renderSlot } from './astro/server_DxtXFKMS.mjs';
-/* empty css                         */
-
-const $$Page = createComponent(($$result, $$props, $$slots) => {
-  return renderTemplate`${maybeRenderHead()}<main> ${renderSlot($$result, $$slots["default"])} </main>`;
-}, "/home/bomboonsan/Desktop/Astro/onlygroub-pnpm/src/templates/Page.astro", void 0);
-
-export { $$Page as $ };
