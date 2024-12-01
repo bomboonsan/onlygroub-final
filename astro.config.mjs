bd4378154510f488b2a1af9173820ab211d04959
bomboonsan <bomboonsan@gmail.com>
2024-12-01 08:46:48 +0700

    removeMovie

--- astro.config.mjs
+++ astro.config.mjs
@@ -28,5 +28,71 @@ export default defineConfig({
         },
       }),
     ],
+    redirects: {
+      '/movie': {
+        status: 301,
+        destination: 'https://movie.onlygroub.com/'
+      },
+      '/category/thai/': {
+        status: 301,
+        destination: 'https://movie.onlygroub.com/category/thai/'
+      },
+      '/category/inter/': {
+        status: 301,
+        destination: 'https://movie.onlygroub.com/category/inter/'
+      },
+      '/four-hundred-and-four-suk-ni-run-run-run': {
+        status: 301,
+        destination: 'https://movie.onlygroub.com/movie/four-hundred-and-four-suk-ni-run-run-run/'
+      },
+      '/wai-nhum-2544': {
+        status: 301,
+        destination: 'https://movie.onlygroub.com/movie/wai-nhum-2544/'
+      },
+      '/red-one': {
+        status: 301,
+        destination: 'https://movie.onlygroub.com/movie/red-one/'
+      },
+      '/police-modify': {
+        status: 301,
+        destination: 'https://movie.onlygroub.com/movie/police-modify/'
+      },
+      '/endpresso': {
+        status: 301,
+        destination: 'https://movie.onlygroub.com/movie/endpresso/'
+      },
+      '/woman-of-the-hour': {
+        status: 301,
+        destination: 'https://movie.onlygroub.com/movie/woman-of-the-hour/'
+      },
+      '/the-paradise-of-thorns': {
+        status: 301,
+        destination: 'https://movie.onlygroub.com/movie/the-paradise-of-thorns/'
+      },
+      '/tee-yod-2': {
+        status: 301,
+        destination: 'https://movie.onlygroub.com/movie/tee-yod-2/'
+      },
+      '/the-4-danger': {
+        status: 301,
+        destination: 'https://movie.onlygroub.com/movie/the-4-danger/'
+      },
+      '/the-substance': {
+        status: 301,
+        destination: 'https://movie.onlygroub.com/movie/the-substance/'
+      },
+      '/luang-phee-teng-comeback': {
+        status: 301,
+        destination: 'https://movie.onlygroub.com/movie/luang-phee-teng-comeback/'
+      },
+      '/uranus2324': {
+        status: 301,
+        destination: 'https://movie.onlygroub.com/movie/uranus2324/'
+      },
+      '/present-still-perfect': {
+        status: 301,
+        destination: 'https://movie.onlygroub.com/movie/present-still-perfect/'
+      },
+    },
     output: 'static',
 });
